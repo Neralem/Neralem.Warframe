d0731519abb2ddbb7eb72d09791e86a4781dee4f
Cekayy95 <61753327+Cekayy95@users.noreply.github.com>
2021-05-06 20:05:00 +0200

    Anzeigen der Items beim Starten

--- MarketCrawler/ViewModels/MainVm.cs
+++ MarketCrawler/ViewModels/MainVm.cs
@@ -235,6 +235,7 @@ namespace MarketCrawler.ViewModels
                 if (value != items)
                 {
                     items = value;
+                    FilteredItems = FilterItems();
                     OnPropertyChanged();
                     
                 }
